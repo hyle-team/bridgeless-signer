429d01b2ac42f5a298aab1fc5f37fc66bc61086e
Mark Cherepovskyi <mark.cherepovskyi@gmail.com>
2024-10-04 13:41:23 +0300

    update migrations

--- internal/assets/migrations/004_signature.sql
+++ internal/assets/migrations/004_signature.sql
@@ -1,9 +1,9 @@
 -- +migrate Up
 
 ALTER TABLE deposits
-    ADD COLUMN signature text, ADD COLUMN tx_nonce int;
+    ADD COLUMN signature text;
 
 -- +migrate Down
 
 ALTER TABLE deposits
-    DROP COLUMN signature, DROP COLUMN tx_nonce;
+    DROP COLUMN signature;
